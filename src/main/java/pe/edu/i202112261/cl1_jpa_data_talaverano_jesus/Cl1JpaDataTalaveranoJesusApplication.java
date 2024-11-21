61fa1e1a4c386fcf201950a74d157d6a81c64e65
Jesus Talaverano <eliastalaverano@gmail.com>
2024-11-21 09:13:19 -0500

    examen1

---
+++ src/main/java/pe/edu/i202112261/cl1_jpa_data_talaverano_jesus/Cl1JpaDataTalaveranoJesusApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.i202112261.cl1_jpa_data_talaverano_jesus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JpaDataTalaveranoJesusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JpaDataTalaveranoJesusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/i202112261/cl1_jpa_data_talaverano_jesus/Cl1JpaDataTalaveranoJesusApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.i202112261.cl1_jpa_data_talaverano_jesus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JpaDataTalaveranoJesusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
